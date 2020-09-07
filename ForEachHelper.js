1cc08de9d585b535ddf0509c4e809638f37f4dcf
Hemant Wagh <hemantwagh07@gmail.com>
2020-09-07 12:28:42 +0530

    Update ForEachHelper.js
    Example with Named function

--- ForEachHelper.js
+++ ForEachHelper.js
@@ -22,3 +22,22 @@ green
 blue
 
 */
+
+// Using named function 
+
+let numbers = [1, 2, 3, 4, 5];
+
+let sum = 0;
+
+//Function named as adder
+var adder = (number) => {
+    sum += number;
+}
+
+//Pass adder function to foreach
+numbers.forEach(adder);
+
+// Print sum variable
+console.log(sum);
+// Output
+// 15
